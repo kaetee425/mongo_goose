4004ed52081bdb97cc72d33af853bfda4dd18347
kaetee425 <kingpang@outlook.com>
2017-12-20 14:49:57 -0800

    update app.js

--- server.js
+++ server.js
@@ -22,19 +22,48 @@ mongoose.connect("mongodb://localhost/goGoose", {
 });
 
 app.get("/scape", function(req, res){
-	axios.get("")
+	axios.get("http://www.echojs.com/").then(function(response){
+		var $ = cheerio.load(response.data);
+
+		$("article h2").each(function(i, element){
+			var result = {};
+
+			result.title = $(this).children("a").text();
+			result.link = $(this).children("a").attr('href');
+
+			db.Article.create(result).then(function(dbArticle){
+				res.send("Scrape Complete");
+			}).catch(function(err){
+				res.json(err);
+			})
+		});
+	});
 });
 
 app.get("/articles", function(req, res){
-
+	db.Article.find({}).then(function(dbArticle){
+		res.json(dbArticle);
+	}).catch(function(err){
+		res.json(err);
+	});
 });
 
 app.get("/articles/:id", function(req, res){
-
+	db.Article.findOne({_id: req.params.id}).populate("note").then(function(dbArticle){
+		res.json(dbArticle);
+	}).catch(function(err){
+		res.json(err)
+	})
 });
 
 app.post("/articles/:id", function(req, res){
-
+	db.Note.create(req.body).then(function(dbNote){
+		return db.Article.findOneAndUpdate({_id: req.params.id}, {note: dbNote._id}, {new: true});
+	}).then(function(dbArticle){
+		res.json(dbArticle);
+	}).catch(function(err){
+		res.json(err)
+	})
 });
 
 app.listen(PORT, function(){
