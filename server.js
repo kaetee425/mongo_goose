80cc2a6b318afcb9744666e8b997d73ed82d5965
dblanchard13 <davidb.prof@gmail.com>
2018-01-10 15:42:20 -0800

    Homework comments.

--- server.js
+++ server.js
@@ -21,22 +21,32 @@ mongoose.connect("mongodb://localhost/goGoose", {
 	useMongoClient: true
 });
 
-app.get("/scape", function(req, res){
+app.get("/scrape", function(req, res){
 	axios.get("http://www.cracked.com/humor-movies-tv.html").then(function(response){
 		var $ = cheerio.load(response.data);
 
 		$("div.content-card").each(function(i, element){
 			var result = {};
 
+			// seems like these aren't consistently returning the values you're
+			// expecting which is causing the below create call to fail when either
+			// title or link is missing. So you should probably validate that both
+			// are present before trying to create a new article.
 			result.title = $(this).children("div.content-card-content a").text();
 			result.link = $(this).children("div.content-card-content a").attr('href');
 
 			db.Article.create(result).then(function(dbArticle){
-				res.send("Scrape Complete");
+				console.log('Created an article: ', dbArticle)
 			}).catch(function(err){
+				// It's good practice to log out errors so that the server logs can be used
+				// when debugging issues.
+				console.log('Error creating article: ', err)
 				res.json(err);
 			})
 		});
+		// You want this method call outside of the each loop so you don't get an 
+		// error for trying to send multiple responses to the same request.
+		res.send("Scrape Complete");
 	});
 });
 
